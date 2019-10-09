2d1bd2c1ee124d0a8cf3a445f43006cf83b29077
Mubashir Iqbal <bc140400342@gmail.com>
2019-10-09 22:34:07 +0500

    jsut commiting

---
+++ array.cpp
@@ -0,0 +1,139 @@
+#include "array.h"
+
+Array::Array()
+{
+
+}
+
+Array::Array(int size)
+{
+
+}
+Array::Array(int *arr, int size)
+{
+
+}
+Array::Array(const Array &)
+{
+
+}
+Array::~Array()
+{
+
+}
+
+//member function
+int Array::getAt(int i)
+{
+
+}
+
+void Array::setAt(int i, int val)
+{
+
+}
+
+Array Array::subArr(int pos, int size)
+{
+
+}
+
+Array Array::subArr(int pos)
+{
+
+}
+
+int * Array::subArrPointer(int pos, int size)
+{
+
+}
+
+int * Array::subArrPointer(int pos)
+{
+
+}
+
+void Array::push_back(int a)
+{
+
+}
+
+int Array::pop_back()
+{
+
+}
+
+int Array::inset(int index, int val)
+{
+
+}
+
+int Array::erase(int index, int val)
+{
+
+}
+
+void Array::size()
+{
+
+}
+
+int Array::length()
+{
+
+}
+
+void Array::clear()
+{
+
+}
+
+int Array::value(int index)
+{
+
+}
+
+void Array::assign(int index, int val)
+{
+
+}
+
+void Array::copy(const Array & arr)
+{
+
+}
+
+void Array::copy(const int * arr, int size)
+{
+
+}
+
+void Array::display()
+{
+
+}
+
+bool Array::isEmpty()
+{
+
+}
+Array Array::find(int i)
+{
+    // return the indexes of integer searched in the array
+}
+
+bool Array::equal(Array)
+{
+
+}
+
+int Array::sort()
+{
+
+}
+
+void Array::reverse()
+{
+
+}
+
---
+++ array.h
@@ -0,0 +1,47 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+
+class Array
+{
+private:
+    int *intPtrArray;
+    int intArray;
+
+public:
+    Array();
+    Array(int size);
+    Array(int *arr, int size);
+    Array(const Array &);
+    ~Array();
+
+    //member function
+    int getAt(int i);
+    void setAt(int i, int val);
+    Array subArr(int pos, int size);
+    Array subArr(int pos);
+    int *subArrPointer(int pos, int size);
+    int *subArrPointer(int pos);
+    void push_back(int a);
+    int pop_back();
+    int inset(int index, int val);
+    int erase(int index, int val);
+    void size();
+    int length();
+    void clear();
+    int value(int index);
+    void assign(int index, int val);
+    void copy(const Array & arr);
+    void copy(const int * arr, int size);
+    void display();
+    bool isEmpty();
+    Array find(int i); // return the indexes of integer searched in the array
+    bool equal(Array);
+    int sort();
+    void reverse();
+
+
+
+};
+
+#endif // ARRAY_H
